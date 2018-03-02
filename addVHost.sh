b116ef611e0d260a099fcf52fc4a5595d4d56a47
COSTEMaxime <maxime.coste2@hotmail.fr>
2018-03-02 16:13:52 +0100

    VHost script

---
+++ addVHost.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+domainName="cesi.local"
+email='testmail@test.fr'
+rootDir='/etc/apache2/sites-available'
+documentRoot='/var/www/test.local'
+
+if ! [ -d $rootDir]; then
+	mkdir $rootDir
+	chmod 777 $rootDir
+
+	echo "
+<VirtualHost *:80>
+
+	ServerAdmin $email
+	ServerName $domainName
+	ServerAlias $domainName
+
+	DocumentRoot $documentRoot
+
+		ErrorLog /var/log/apache2/error.log
+
+	LogLevel warn
+
+	CustomLog /var/log/apache2/access.log combined
+
+   </VirtualHost>" >> $rootDir/$domainName.conf
+
+	echo "127.0.0.1		$domainName" >> /etc/hosts
+	a2ensite $domainName.conf
+	/etc/init.d/apache2 reload
+
+	echo "
+<html>
+	<head>
+		<title>Site cesi.local</title>
+	</head>
+		
+	<body>
+		<h1>Bienvenue sur le site cesi.local !</h1>
+	</body>
+</html>" >> $documentRoot/index.html
+	
+	exit 0;
+fi
